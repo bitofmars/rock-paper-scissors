7f7f152646a3245d721cd98d6703764665198bee
Martina Peluso <bitfmars@gmail.com>
2024-07-26 21:51:54 -0300

    Create a function that starts the game.
    The variables for scores and choices, as well as the playRound function, were moved inside this one.

--- script.js
+++ script.js
@@ -13,22 +13,25 @@ function getHumanChoice(){
     return prompt("Rock, paper or scissors?");
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+function playGame(){
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
 
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanSelection, computerSelection){
-    if (humanSelection === computerSelection){
-        console.log("It's a tie! You both chose " + humanSelection + ".");
-    }else if ((humanSelection === "rock" && computerSelection === "scissors") ||
-    (humanSelection === "paper" && computerSelection === "rock") ||
-    (humanSelection === "scissors" && computerSelection === "paper")){
-        console.log("You won! " + humanSelection + " beats " + computerSelection + ".");
-        humanScore++;
-    }else {
-        console.log("You lost! " + computerSelection + " beats " + humanSelection + ".");
-        computerScore++;
+    function playRound(humanSelection, computerSelection){
+        if (humanSelection === computerSelection){
+            console.log("It's a tie! You both chose " + humanSelection + ".");
+        }else if ((humanSelection === "rock" && computerSelection === "scissors") ||
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper")){
+            console.log("You won! " + humanSelection + " beats " + computerSelection + ".");
+            humanScore++;
+        }else {
+            console.log("You lost! " + computerSelection + " beats " + humanSelection + ".");
+            computerScore++;
+        }
     }
+
 }
